d68d1c57823220748ff43ccb98a6c35f95356bf7
babisUkiyo <c.kolokouris@adrestia.eu>
2024-04-10 11:17:09 +0300

    Update file rendering in app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.static(path.join(__dirname, '/')));
 // Route to serve the HTML file
 app.get('/', (req, res) => {
-    res.sendFile('index.html');
+    res.render('index.html');
 });
 
 app.listen(PORT, () => {
