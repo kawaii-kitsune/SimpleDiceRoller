24ed77cc4dfce847bee7105637b3270da952aaa3
Charalampos Kolokouris <c.kolokouris@adrestia.eu>
2025-02-07 12:01:02 +0200

    Add SVG icons for dice faces and update static file serving paths

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 3000;
 
 // Serve static files
 app.use(express.static(path.join(__dirname, '/dist')));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/public', express.static(path.join(__dirname, '/public/dice.svg/icons/000000/transparent/1x1')));
 app.use(express.static(path.join(__dirname, '/')));
 // Route to serve the HTML file
 app.get('/', (req, res) => {
