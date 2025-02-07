4b984468a6f72376c2174a3ba7ce3ebdc7bb347d
Charalampos Kolokouris <c.kolokouris@adrestia.eu>
2025-02-07 12:07:09 +0200

    Update static file serving paths for dice icons in app.js and index.html

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 3000;
 // Serve static files
 app.use(express.static(path.join(__dirname, '/dist')));
 app.use(express.static(path.join(__dirname, '/public')));
+app.use('/public/icons', express.static(path.join(__dirname, '/public/dice.svg/icons/000000/transparent/1x1')));
+
 app.use('/public', express.static(path.join(__dirname, '/public/dice.svg/icons/000000/transparent/1x1')));
 app.use(express.static(path.join(__dirname, '/')));
 // Route to serve the HTML file
